711ad29111dc90812568d2dc4356078606da6dbb
Avani <avani.jadeja@gmail.com>
2023-06-04 09:04:25 -0500

    Set index file for controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,16 @@
 // main route file
 
+// require express
 const router = require("express").Router();
 
+// require for api , home-routes
+const homeRoutes = require("./home-routes.js");
 const apiRoutes = require("./api");
 
+// set routes / for homeRoutes
+router.use("/", homeRoutes);
+
+// set routes /api for apiRoutes
 router.use("/api", apiRoutes);
 
 module.exports = router;
