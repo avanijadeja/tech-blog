0ff275ec3ff73e75dc9d38f8f721d19e74a5d99e
Avani <avani.jadeja@gmail.com>
2023-06-04 09:50:31 -0500

    Add dashboard routes in index.js file in controllers

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,10 @@
 // require express
 const router = require("express").Router();
 
-// require for api , home-routes
+// require for api , home-routes, dashboard-routes
 const homeRoutes = require("./home-routes.js");
 const apiRoutes = require("./api");
+const dashboardRoutes = require("./dashboard-routes.js");
 
 // set routes / for homeRoutes
 router.use("/", homeRoutes);
@@ -13,4 +14,8 @@ router.use("/", homeRoutes);
 // set routes /api for apiRoutes
 router.use("/api", apiRoutes);
 
+// set routes /dashboard for dashboardRoutes
+router.use("/dashboard", dashboardRoutes);
+
+// export router
 module.exports = router;
