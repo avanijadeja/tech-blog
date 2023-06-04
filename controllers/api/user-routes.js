b2737f242933cdc671d3ff9af0afc6ce2e6ed91f
Avani <avani.jadeja@gmail.com>
2023-06-03 20:29:53 -0500

    Created user-routes.js file for login signup and logout in controllers folder

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,9 +1,63 @@
-// main route file
-
 const router = require("express").Router();
+const { User } = require("../../models");
 
-const apiRoutes = require("./api");
+// SIGN UP
+router.post("/signup", async (req, res) => {
+  try {
+    const newUser = await User.create({
+      userName: req.body.userName,
+      password: req.body.password,
+    });
+    console.log("NewUser:" + newUser.userId);
+    req.session.save(() => {
+      req.session.userId = newUser.id;
+      req.session.userName = newUser.userName;
+      req.session.loggedIn = true;
 
-router.use("/api", apiRoutes);
+      res.json(newUser);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        userName: req.body.userName,
+      },
+    });
+    if (!user) {
+      res.status(400).json({ message: "No user account found!" });
+      return;
+    }
+    const validPassword = user.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: "No user account found!" });
+      return;
+    }
+    req.session.save(() => {
+      req.session.userId = user.id;
+      req.session.userName = user.userName;
+      req.session.loggedIn = true;
+      res.json({ user, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(400).json({ message: "No user account found!" });
+  }
+});
+
+// LOGOUT
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+// main route file
+
+const router = require("express").Router();
+
+const apiRoutes = require("./api");
+
+router.use("/api", apiRoutes);
+
+module.exports = router;
